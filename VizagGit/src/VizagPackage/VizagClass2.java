75cc1013946fabfbdcce8cb2705a7723a3338f49
DELL <DELL@DESKTOP-DIQ1OMU>
2022-10-29 18:18:12 +0530

    Second correction updated

--- VizagGit/src/VizagPackage/VizagClass2.java
+++ VizagGit/src/VizagPackage/VizagClass2.java
@@ -3,7 +3,7 @@ package VizagPackage;
 public class VizagClass2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Navaz shariff class 2 uploaded");
 
 	}
 
